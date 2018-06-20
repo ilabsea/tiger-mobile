56962a5f28efb83abe38dcc7312427fbf9a8c6c2
Sokly <hengsokly23@gmail.com>
2018-06-20 15:56:14 +0700

    change environment

--- app/environments/environment.js
+++ app/environments/environment.js
@@ -1,4 +1,6 @@
 export const environment = {
-  apiUrl: 'http://192.168.1.107:3000/api/v1/',
-  domain: 'http://192.168.1.107:3000'
+  // apiUrl: 'http://192.168.1.107:3000/api/v1',
+  // domain: 'http://192.168.1.107:3000'
+  apiUrl: 'http://110.74.204.121:8282/app/api/v1',
+  domain: 'http://110.74.204.121:8282/app'
 };
